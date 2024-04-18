a0b373a5965c12f3106ee36b0788202815a32563
Angel Almanza <almanza.angel245@gmail.com>
2024-04-18 08:24:05 -0700

    Added Header component for mobile devices

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from '../pages/homePage';
 import { SignInPage } from "../pages/signInPage";
+import { NotFound } from "../pages/notFound";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,44 @@ const router = createBrowserRouter([
   {
     path: '/sign-in',
     element: <SignInPage />,
+  },
+  {
+    path: '/home',
+    element: <HomePage />,
+    children: [
+      {
+        path: '',
+        element: <HomePage />,
+      },
+      {
+        path: 'search',
+        element: <NotFound />,
+      },
+      {
+        path: 'found',
+        element: <NotFound />,
+      },
+      {
+        path: 'adoption',
+        element: <NotFound />,
+      },
+      {
+        path: 'profile',
+        element: <NotFound />,
+      },
+      {
+        path: 'chats',
+        element: <NotFound />,
+      },
+      {
+        path: 'veterinaries',
+        element: <NotFound />,
+      },
+      {
+        path: 'logout',
+        element: <NotFound />,
+      }
+    ]
   }
 ])
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ export default {
         'icon-color': '#A3A3A3',
         'text-primary-color': '#797979',
         'primary-gray': '#EFEFEF',
+        'primary-color': '#5AC3E1',
+        'hover-bg': 'rgba(217, 217, 217, 0.7)',
       },
     },
   },
