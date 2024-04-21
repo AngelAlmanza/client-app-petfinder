457022b2a27291ffc9fe01f6c7c1d1c58a2e1e25
Angel Almanza <almanza.angel245@gmail.com>
2024-04-21 14:55:47 -0700

    Added Posts component

---
+++ src/components/PetPost.tsx
@@ -0,0 +1,61 @@
+import { faEllipsis, faUser, faFlag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ReactNode, useState } from "react";
+import { PostButton } from "./buttons/PostButton";
+
+type Props = {
+  username: string,
+  description: string,
+  postImage?: string,
+  altImage?: string
+  buttonByPublish: ReactNode,
+}
+
+export const PetPost = ({ username, description, postImage, altImage, buttonByPublish }: Props) => {
+  const [opacityButton, setOpacityButton] = useState<string>('opacity-0');
+
+  const hideButton = () => {
+    if (opacityButton === 'opacity-0') {
+      setOpacityButton('opacity-100')
+    } else {
+      setOpacityButton('opacity-0')
+    }
+  };
+
+  return (
+    <article className="w-full rounded-2xl bg-white shadow p-1 flex flex-col">
+      {
+        postImage && (
+          <img
+            src={postImage} alt={altImage}
+            className="w-full h-52 object-cover rounded-2xl"
+          />
+        )
+      }
+      <div className="px-2">
+        <div className="w-100 flex justify-between items-center my-3 relative">
+          <div>
+            <FontAwesomeIcon icon={faUser} className="text-text-primary-color text-xl" />
+            <span className="ml-2 text-sm font-medium text-text-primary-color">{ username }</span>
+          </div>
+          <button onClick={hideButton}>
+            <FontAwesomeIcon icon={faEllipsis} className="text-text-primary-color text-xl" />
+          </button>
+          <div className={`absolute right-0 top-4 transition-all ${opacityButton}`} >
+            <button className="bg-primary-gray rounded-lg shadow-md py-2 px-4 flex items-center">
+              <FontAwesomeIcon icon={faFlag} className="text-danger-color text-xl" />
+              <span className="inline-block ml-2 font-medium text-sm text-text-primary-color">Reportar</span>
+            </button>
+          </div>
+        </div>
+        <p className="mb-4 text-text-primary-color text-sm overflow-hidden h-14 line-clamp-3">
+          { description }
+        </p>
+        <div className="flex justify-between items-center my-2">
+          { buttonByPublish }
+          <PostButton onClick={() => {}} type="more" />
+        </div>
+      </div>
+    </article>
+  )
+}
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,9 +1,26 @@
+import { PetPost } from "../components/PetPost"
+import { PostButton } from "../components/buttons/PostButton"
 import { MainLayout } from "../layouts/MainLayout"
+
+const TEXT_TEST = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, reiciendis! Dolore vero ad delectus consectetur distinctio laboriosam labore amet explicabo adipisci iste, error accusamus eveniet non eius nisi? Recusandae, maxime? Mollitia corporis facere accusamus omnis assumenda saepe modi cupiditate sequi, aperiam sed officiis ut eius cum excepturi vero culpa quo et totam sunt consequatur reiciendis placeat. Officiis molestiae error provident. Dolore ratione quod possimus sequi quidem eum corrupti dolor dignissimos, quo culpa impedit nisi laborum nostrum molestias sint nobis itaque dolorum voluptas odio perspiciatis ipsum beatae? Fuga voluptatum ea sit.";
 
 export const HomePage = () => {
   return (
     <MainLayout>
-      <h1>Home Page</h1>
+      <section className="grid grid-cols-1 gap-4">
+        <PetPost
+          buttonByPublish={<PostButton onClick={() => {}} type="lost" />}
+          username="Username"
+          postImage="https://via.placeholder.com/150"
+          altImage="Post image"
+          description={TEXT_TEST}
+        />
+        <PetPost
+          buttonByPublish={<PostButton onClick={() => {}} type="inAdoption" />}
+          username="Username"
+          description={TEXT_TEST}
+        />
+      </section>
     </MainLayout>
   )
 }
