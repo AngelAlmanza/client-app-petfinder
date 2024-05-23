64fcade3ed1f4deaddfd43bc6049082bc48181de
Angel Almanza <almanza.angel245@gmail.com>
2024-05-23 12:13:11 -0700

    Added Image post placeholder

--- src/utils/getPostImage.ts
+++ src/utils/getPostImage.ts
@@ -1,6 +1,12 @@
 import { Image } from "../interfaces/PostInterfaces";
+import imageNotFound from "../assets/images/image-not-found.jpeg";
 
 export const getPostImage = (post: Image[]) => {
-  if (post.length === 0) return "https://via.placeholder.com/150";
-  return post[0].base64_url;
+  if (!post || post.length === 0) {
+    return imageNotFound;
+  } else if (!post[0].base64_url) {
+    return imageNotFound;
+  } else {
+    return post[0].base64_url;
+  }
 };
